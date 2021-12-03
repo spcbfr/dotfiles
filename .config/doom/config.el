280b136c84ff6d803e399530fc1e7d44330f63a3
spcbfr <youssefbouzekri@protonmail.com>
2021-12-03 23:57:04 +0100

    more configuration for doom

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,3 +1,5 @@
+;;; config.el -*- lexical-binding: t; -*-
+
 (setq user-full-name "spcbfr"
       user-mail-address "spcbfr@protonmail.com")
 
@@ -13,3 +15,22 @@
          (let ((project-name (projectile-project-name)))
            (unless (string= "-" project-name)
              (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
+
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 13) ;; the default general doom font
+      doom-unicode-font (font-spec :family "Fira Code" :size 13) ;; Fallback unicode font for icons, glyphs, etc...
+      doom-variable-pitch-font (font-spec :family "IBM Plex Mono" :size 15) ;; the font to use for variable-pitch text
+      doom-big-font (font-spec :family "Jetbrains Mono" :size 24)) ;; font used in big-font-mode
+
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+(map! :map +doom-dashboard-mode-map
+      :ne "f" #'find-file
+      :ne "r" #'consult-recent-file
+      :ne "p" #'doom/open-private-config
+      :ne "v" #'vterm/here
+      :ne "c" (cmd! (find-file (expand-file-name "config.org" doom-private-dir)))
+      :ne "q" #'save-buffers-kill-terminal)
+
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
